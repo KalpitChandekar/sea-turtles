f1d5872e0d54ae82a25b7a07f817a4f92cfea180
Kalpit <kalpitchandekar1736@gmail.com>
2024-09-26 11:36:13 +0530

    add all wanted components

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
 import Hero from "@/components/Hero";
 import Navbar from "../components/Navbar";
 import Fact1 from "@/components/Fact1";
+import Fact2 from "@/components/Fact2";
+import Footer from "@/components/Footer";
+import Fact3 from "@/components/Fact3";
 
 const page = () => {
   return (
@@ -8,6 +11,9 @@ const page = () => {
       <Navbar />
       <Hero />
       <Fact1 />
+      <Fact2 />
+      <Fact3 />
+      <Footer />
     </main>
   );
 };
