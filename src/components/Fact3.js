afe8bf4c6b7bfde2d14ca15105b729b4a59e4e7e
Kalpit <kalpitchandekar1736@gmail.com>
2024-09-26 11:34:49 +0530

    [feat]: create last fact

---
+++ src/components/Fact3.js
@@ -0,0 +1,20 @@
+const Fact3 = () => {
+  return (
+    <div className="flex mt-72 max-w-6xl mx-auto">
+      <div className="w-1/2"></div>
+      <div className="w-1/2 mx-auto">
+        <h1 className="text-9xl font-semibold text-[#F2FFFC] ">O3</h1>
+
+        <h2 className="my-3 text-6xl font-semibold">Going home to <br /> nest</h2>
+
+        <p className="w-[80%]">
+          When it's time to lay eggs, female sea turtles take a long trip back
+          to where they were born. It's a big circle-of-life moment. They lay
+          their eggs on the beach, cover them up, and then head back to their
+          feeding grounds.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Fact3;
