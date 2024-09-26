19a1efeba9b581e601e123dee447229810b076e6
Kalpit <kalpitchandekar1736@gmail.com>
2024-09-26 12:08:28 +0530

    update button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 const Button = () => {
   return (
-    <div className="bg-[#00FFB2] py-2 px-8 rounded-full text-black text-sm font-semibold">
+    <div className="bg-[#00FFB2] py-3 text-center rounded-full text-black text-sm font-semibold w-36 my-6">
       Learn more
     </div>
   );
